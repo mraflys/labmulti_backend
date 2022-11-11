02d0a5dbc12fb6b4147fa6ff6a31ff13b57c83b4
Rafly <rafly.syahputra@usenobi.com>
2022-11-11 14:53:36 +0700

    fix curs

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "*",
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 };
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
